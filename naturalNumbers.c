9c8cfa5ae4977f5b5a087710d985bc2ec8c948e6
ifthekar <sajalcse2018@gmail.com>
2023-06-14 09:07:14 +0600

    C program to print all natural numbers from 1 to n

---
+++ naturalNumbers.c
@@ -0,0 +1,22 @@
+/*
+
+C program to print all natural numbers from 1 to n
+
+*/
+#include<stdio.h>
+int main(){
+
+
+  int i,iteration;
+  printf("Enter Your Value How Many Times You Want=");
+  scanf("%d",&iteration);
+  for(i=1;i<=iteration;i++){
+    printf("%d\n",i);
+  }
+
+
+
+return 0;
+}
+
+
