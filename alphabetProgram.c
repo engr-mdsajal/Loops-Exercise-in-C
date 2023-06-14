1a3f5de1a3f1d1e59f76d06b98214a2b3ae377fb
ifthekar <sajalcse2018@gmail.com>
2023-06-14 15:19:49 +0600

    C program to print alphabets from a to z

---
+++ alphabetProgram.c
@@ -0,0 +1,16 @@
+/*
+C program to print alphabets from a to z
+*/
+
+#include<stdio.h>
+int main(){
+
+  char ch;
+  printf("Alphabets From a to z= \n");
+  for(ch='a';ch<='z';ch++){
+
+    printf("%c\n",ch);
+  }
+
+return 0;
+}
